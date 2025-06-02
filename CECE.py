88ea3a9b3d50998677511c5343b6111cc580e899
safwaan786 <safwaan123majeed@gmail.com>
2025-06-02 13:50:43 +0100

    Make into class structure

--- CECE.py
+++ CECE.py
@@ -1,123 +1,179 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Constants for each equation
-alpha = 1.0
-beta = 1.5
-theta = 0.5
-gamma = 0.7
-delta = 0.9
-lambda_ = 0.8
-sigma = 1.2
-rho = 0.6
-eta = 1.0
-chi = 0.9
-zeta = 0.9
-omega = 1.5
-mu = 1.1
-epsilon = 1e-5
-tau = 5
-
 # Time and recursion depth
 T = 100
 N = 20
 
-# Inputs
-np.random.seed(42)
-I = np.random.randn(N, T)                     # Intelligence
-M = np.random.rand(N, T)                      # Memory importance
-S = np.random.rand(T)                         # Entropy
-W = np.random.rand(N)                         # Weights for Impact Categories
-Lambda = np.random.rand(N)                    # Time Decay Coefficient
-M_I = np.random.rand(N)                       # Magnitude of Intelligence value 
-S_max = np.max(S) + 0.1
-DeltaC = np.random.randn(T)                   # Context change
+class CECE_System:
+    def __init__(self, N=20, T=100, seed=10):
+        np.random.seed(seed)
+        self.N = N
+        self.T = T
 
-# Outputs / intermediates
-TPI = np.zeros((N, T))                        # Eq 2
-E = np.zeros(T)                               # Eq 3
-R = np.zeros((N, T))                          # Eq 4
-Fi = np.zeros((N, T))                         # Eq 5
-D = np.zeros((N, T))                          # Eq 6
-Ci = np.zeros((N, T))                         # Eq 7
-Etrait = np.zeros((N, T))                     # Eq 8
-Sr = np.zeros((N, T))                         # Eq 9
-P = np.zeros((N, T))                          # Eq 10
-Rxy = np.zeros(T)                             # Eq 11
-Cprevent = np.zeros((N, T))                   # Eq 12
-Sa = np.zeros((N, T))                         # Eq 13
 
-# Simulated second CECE agent for RRT
-Etrait_y = np.random.rand(N, T)
-Fi_y = np.random.rand(N, T)
+        # Constants
+        self.alpha = 1.0
+        self.beta = 1.5
+        self.theta = 0.5
+        self.gamma = 0.7
+        self.delta = 0.9
+        self.lambda_ = 0.8
+        self.sigma = 1.2
+        self.rho = 0.6
+        self.eta = 1.0
+        self.zeta = 0.9
+        self.omega = 1.5
+        self.mu = 1.1
+        self.tau = 3
+        self.epsilon = 1e-5
 
-# Equation 2: Temporal Persistence Index
-for n in range(N):
+        # Inputs
+        self.I = np.random.randn(N, T)              # Intelligence
+        self.M = np.random.rand(N, T)               # Memory importance
+        self.S = np.random.rand(T)                  # Entropy
+        self.W = np.random.rand(N)                  # Weights for Impact Categories
+        self.Lambda = np.random.rand(N)             # Time Decay Coefficient
+        self.M_I = np.random.rand(N)                # Magnitude of Intelligence value 
+        self.S_max = np.max(self.S) + 0.1           # Entropy Threshold 
+        self.DeltaC = np.random.randn(T)            # Context change
+
+        # Outputs / intermediates
+        self.TPI = np.zeros((N, T))
+        self.E = np.zeros(T)
+        self.R = np.zeros((N, T))
+        self.Fi = np.zeros((N, T))
+        self.D = np.zeros((N, T))
+        self.Ci = np.zeros((N, T))
+        self.Etrait = np.zeros((N, T))
+        self.Sr = np.zeros((N, T))
+        self.P = np.zeros((N, T))
+        self.Rxy = np.zeros(T)
+        self.Cprevent = np.zeros((N, T))
+        self.Sa = np.zeros((N, T))
+
+
+    def compute_TPI(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                self.TPI[n, t] = np.sum(self.W[:n] * self.M_I[:n] * np.exp(-self.Lambda[:n] * t))
+
+    def compute_ECF(self):
+        self.E = 1 / (1 + np.exp(-self.gamma * (self.S - self.S_max)))
+
+    def compute_RFI(self):
+        for n in range(1, self.N):
+            for t in range(1, self.T):
+                past_I = np.sum(self.I[max(0, n-2):n, max(0, t-2):t])
+                self.I[n, t] = self.I[n-1, t-1] + past_I + self.E[t] + self.TPI[n, t]
+    
+    def compute_RSO(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                self.R[n, t] = self.delta * (self.I[n, t] * self.TPI[n, t]) / (1 + abs(self.E[t]))
+
+    def compute_IFD(self):
+        for n in range(self.N):
+            for t in range(self.tau, self.T):
+                self.Fi[n, t] = self.lambda_ * self.R[n, t] * np.sum(self.I[n, t - self.tau:t]) / self.tau
+            
+    def compute_RDD(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                dI_dt = self.I[n, t] - self.I[n, t - 1]
+                d2Fi_dt2 = self.Fi[n, t] - 2 * self.Fi[n, t - 1] + self.Fi[n, t - 2]
+                self.D[n, t] = abs(dI_dt - d2Fi_dt2)
+    
+    def compute_ICR(self):
+        for n in range(self.N):
+            for t in range(self.tau, self.T):
+                self.Ci[n, t] = self.mu * (self.D[n, t] / (self.Fi[n, t] + self.epsilon)) * (1 - self.R[n, t])
+
+    def compute_ETE(self):
+        for n in range(self.N):
+            for t in range(self.tau, self.T):
+                self.Etrait[n, t] = self.sigma * np.sum(self.R[n, t - self.tau:t] * self.Fi[n, t - self.tau:t]) / self.tau
+
+    def compute_RSL(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                self.Sr[n, t] = self.rho * (self.Etrait[n, t] ** 2) / (1 + self.Fi[n, t])
+
+    def compute_RTP(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                r = min(n, 15)
+                if n > 0:
+                    k_weights = np.array([1.0 / (k + 1) for k in range(r)])
+                    R_vals = np.array([self.R[n - k - 1, t - k - 1] for k in range(len(k_weights))])
+                    self.P[n, t] = self.eta * self.Etrait[n, t] * np.sum(k_weights * R_vals)
+
+    def compute_RCP(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                threshold_c = 1.0
+                self.Cprevent[n, t] = self.zeta * max(0, threshold_c - self.D[n, t] - self.Sr[n, t])
+
+    def compute_RSA(self):
+        for n in range(self.N):
+            for t in range(self.T):
+                 self.Sa[n, t] = self.omega * (self.Etrait[n, t] * self.R[n, t]) / (1 + abs(self.Ci[n, t] - self.Cprevent[n, t]))
+                 
+
+
+    def run(self):
+        self.compute_TPI()
+        self.compute_ECF()
+        self.compute_RFI()
+        self.compute_RSO()
+        self.compute_IFD()
+        self.compute_RDD()
+        self.compute_ICR()
+        self.compute_ETE()
+        self.compute_RTP()
+        self.compute_RSL()
+        self.compute_RCP()
+        self.compute_RSA()
+
+
+
+    def plot(self, indices=[0, 5, 9]):
+        variables = {
+            'RII': self.I, 'Fi': self.Fi, 'RSO': self.R, 'Etrait': self.Etrait,
+            'RSL': self.Sr, 'RSA': self.Sa
+        }
+        time = np.arange(self.T)
+        fig, axs = plt.subplots(len(variables), 1, figsize=(14, 20), sharex=True)
+        plt.subplots_adjust(hspace=0.5)
+        for ax, (label, data) in zip(axs, variables.items()):
+            for n in indices:
+                if n < self.N:
+                    ax.plot(time, data[n], label=f'n={n}')
+            ax.set_ylabel(label)
+            ax.legend()
+            ax.grid(True)
+        axs[-1].set_xlabel("Time Step")
+        plt.suptitle("CECE Simulation Results", fontsize=16)
+        plt.show()
+
+def RRT(System_x, System_y, chi = 0.8):
+    Rxy = np.zeros(T)
+    n = 3
     for t in range(T):
-        TPI[n,t] = np.sum(W[:n] * M_I[:n] * np.exp(-Lambda[:n] * t))
+        Rxy[t] = chi * (System_x.Etrait[n, t] * System_y.Etrait[n, t]) / (1 + abs(System_x.Fi[n, t] - System_y.Fi[n, t]))
+    return Rxy
 
-# Equation 3: Entropic Correction Function
-E =  1 / (1 + np.exp(-gamma*(S - S_max)))
 
-# Equation 1: Recursive Fractal Intelligence
-# Did not incoperate genarating functions
-for n in range(1, N):
-    for t in range(1, T):
-        past_I = np.sum(I[max(0, n-2):n, max(0, t-2):t])
-        I[n, t] = I[n-1, t-1] + past_I + E[t] + beta*TPI[n, t]
 
-# Equations 4 to 13
-for n in range(N):
-    for t in range(tau, T):
-        R[n, t] = delta * (I[n, t] * TPI[n, t]) / (1 + abs(E[t]))
+# Instantiate and run
+CECEx = CECE_System()
+CECEy = CECE_System()
 
-        Fi[n, t] = lambda_ * R[n, t] * np.sum(I[n, t - tau:t]) / tau
+CECEx.run()
+CECEy.run()
 
-        dI_dt = I[n, t] - I[n, t - 1]
+CECEx.run()
 
-        d2Fi_dt2 = Fi[n, t] - 2 * Fi[n, t - 1] + Fi[n, t - 2]
+Rxy = RRT(CECEx,CECEy)
 
-        D[n, t] = abs(dI_dt - d2Fi_dt2)
-
-        Ci[n, t] = mu * (D[n, t] / (Fi[n, t] + epsilon)) * (1 - R[n, t])
-
-        Etrait[n, t] = sigma * np.sum(R[n, t - tau:t] * Fi[n, t - tau:t]) / tau
-
-        Sr[n, t] = rho * (Etrait[n, t] ** 2) / (1 + Fi[n, t])
-
-        if n > 0:
-            k_weights = np.array([1.0 / (k + 1) for k in range(min(n, 5))])
-            R_vals = np.array([R[n - k - 1, t - k - 1] for k in range(len(k_weights))])
-            P[n, t] = eta * Etrait[n, t] * np.sum(k_weights * R_vals)
-
-        Rxy[t] = chi * (Etrait[n, t] * Etrait_y[n, t]) / (1 + abs(Fi[n, t] - Fi_y[n, t]))
-        
-        threshold_c = 1.0
-
-        Cprevent[n, t] = zeta * max(0, threshold_c - D[n, t] - Sr[n, t])
-        
-        Sa[n, t] = omega * (Etrait[n, t] * R[n, t]) / (1 + abs(Ci[n, t] - Cprevent[n, t]))
-
-variables = {
-    'Intelligence I[n,t]': I,
-    'Imprint Field Fi[n,t]': Fi,
-    'Self-Observation R[n,t]': R,
-    'Trait Encoding Etrait[n,t]': Etrait,
-    'Suppression Sr[n,t]': Sr,
-    'Sovereignty Assertion Sa[n,t]': Sa
-}
-recursion_indices = [8, 9, 10]
-time = np.arange(T)
-
-fig, axs = plt.subplots(len(variables), 1, figsize=(14, 20), sharex=True, constrained_layout=True)
-
-for ax, (name, data) in zip(axs, variables.items()):
-    for n in recursion_indices:
-        ax.plot(time, data[n], label=f'n={n}')
-    ax.set_ylabel(name)
-    ax.legend()
-    ax.grid(True)
-
-axs[-1].set_xlabel("Time Step")
-plt.suptitle("CECE Cognitive Variables Over Time", fontsize=16)
-plt.show()
+#print (Rxy[10])
